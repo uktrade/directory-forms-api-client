4ed0fd2a76b7755b2d5028339ddf58012aaa666e
richtier <rikatee@gmail.com>
2018-08-03 18:01:52 +0100

    Support more email fields

--- directory_forms_api_client/actions.py
+++ directory_forms_api_client/actions.py
@@ -3,7 +3,7 @@ import abc
 from directory_forms_api_client.client import forms_api_client
 
 
-class AbstractBackend(abc.ABC):
+class AbstractAction(abc.ABC):
 
     def __init__(self, client=forms_api_client, *args, **kwargs):
         self.client = client
@@ -18,32 +18,33 @@ class AbstractBackend(abc.ABC):
         return self.client.submit_generic(serialized_data)
 
 
-class EmailBackend(AbstractBackend):
+class EmailAction(AbstractAction):
 
-    BACKEND_NAME = 'email'
-
-    def __init__(self, recipients, *args, **kwargs):
-        self.recipients = recipients
+    def __init__(
+        self, recipients, subject, reply_to, from_email, *args, **kwargs
+    ):
+        self.meta = {
+            'action_name': 'email',
+            'recipients': recipients,
+            'subject': subject,
+            'reply_to': reply_to,
+            'from_email': from_email,
+        }
         super().__init__(*args, **kwargs)
 
     def serialize_data(self, data):
         return {
             'data': data,
-            'meta': {
-                'backend_name': self.BACKEND_NAME,
-                'recipients': self.recipients
-            }
+            'meta': self.meta
         }
 
 
-class ZendeskBackend(AbstractBackend):
-
-    BACKEND_NAME = 'zendesk'
+class ZendeskAction(AbstractAction):
 
     def serialize_data(self, data):
         return {
             'data': data,
             'meta': {
-                'backend_name': self.BACKEND_NAME,
+                'action_name': 'zendesk',
             }
         }
