f36a78899b857ef1b60e536ab5eff9b830389c28
Feri Csepregi <feri@31.digital>
2020-10-02 17:01:56 +0100

    fixup! bump django version in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='directory_forms_api_client',
-    version='5.3.1',
+    version='5.4.0',
     url='https://github.com/uktrade/directory-forms-api-client',
     license='MIT',
     author='Department for International Trade',
