4c6e9e5c023fa2e2f928d25e663b011426e9a6d2
Rizwan Mansuri <rizwan@webbyfox.co.uk>
2021-06-08 18:21:46 +0100

    Fix test's delete endpoint

--- tests/test_client.py
+++ tests/test_client.py
@@ -44,9 +44,9 @@ class APIFormsClientTest(TestCase):
         assert 'Authorization' in request.headers
         assert request.headers['Authorization'].startswith('Basic ')
 
-    @stub_request('https://forms.com/api/delete-submission/test@gmail.com/', 'delete')
+    @stub_request('https://forms.com/api/delete-submissions/test@gmail.com/', 'delete')
     def test_delete_form_with_authenticator(self, stub):
-        self.client.delete_submission(
+        self.client.delete_submissions(
             email_address='test@gmail.com', authenticator=basic_authenticator)
 
         request = stub.request_history[0]
