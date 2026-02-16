44e415fc23fce47c3da624740538ed6f1ac87e8f
bobby-didcoding <bobby@didcoding.com>
2026-02-16 16:18:16 +0000

    added new get_forms_api_client function

--- directory_forms_api_client/__init__.py
+++ directory_forms_api_client/__init__.py
@@ -6,6 +6,6 @@ except PackageNotFoundError:
     __version__ = 'dev'
 
 
-from directory_forms_api_client.client import forms_api_client
+from directory_forms_api_client.client import forms_api_client, get_forms_api_client
 
-__all__ = ['forms_api_client']
+__all__ = ['forms_api_client', 'get_forms_api_client']
